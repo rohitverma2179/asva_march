512fdae3080c9a07ae20f1a1f7bbca0a2c59f0cb
sonu7042 <sonusinghmourya100@gmail.com>
2026-04-17 18:32:22 +0530

    updated

--- client/src/components/About/AboutHero.tsx
+++ client/src/components/About/AboutHero.tsx
@@ -3,7 +3,7 @@ import aboutHero from "../../assets/Aboutimages/newimg/aboutHeromain.png";
 
 const AboutHero: React.FC = () => {
   return (
-    <section className="w-full  overflow-hidden py-10 md:py-12 lg:py-28  ">
+    <section className="w-full overflow-hidden pt-20 pb-10 md:pt-20 md:pb-12 lg:pt-28 lg:pb-28">
       <div className="max-w-[1280px] mx-auto px-6 md:px-12 lg:px-20">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 lg:gap-12 items-center">
           
@@ -21,12 +21,12 @@ const AboutHero: React.FC = () => {
           {/* Image Side */}
           <div className="lg:col-span-5 flex justify-center lg:justify-end">
             <div className="relative group">
-              <div className="absolute -inset-4  rounded-[30px] transition-colors duration-500"></div>
+              <div className="absolute -inset-4 rounded-[30px] transition-colors duration-500"></div>
               
               <img
                 src={aboutHero}
                 alt="About Hero"
-                className="relative w-full max-w-[300px] md:max-w-[380px] lg:max-w-[420px] aspect-square object-cover rounded-[20px]  transition-all duration-700 hover:scale-[1.02]"
+                className="relative w-full max-w-[300px] md:max-w-[380px] lg:max-w-[420px] aspect-square object-cover rounded-[20px] transition-all duration-700 hover:scale-[1.02]"
               />
             </div>
           </div>
